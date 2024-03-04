eb2884ba2830057a2e7d84c242acb0d8ef104673
Tyler Walker <boneyardhq@gmail.com>
2024-03-04 12:46:54 -0700

    loop olympics -- Part of silver

---
+++ practice_scratch_assignments/app.js
@@ -0,0 +1,70 @@
+// // // 1. Write a for loop that prints to the console the numbers 0 through 9.
+// for (let i = 0; i <= 9; i++){
+//     console.log(i)
+// }
+// // // 2. Write a for loop that prints to the console 9 through 0.
+// for (let i = 9; i >= 0; i--){
+//     console.log(i)
+// }
+// // // 3. Write a for loop that prints these fruits to the console.`****`
+
+// const fruit = ["banana", "orange", "apple", "kiwi"]
+// for (let i = 0; i < fruit.length; i++){
+//     console.log(fruit[i])
+// }
+
+// // 1. Write a for loop that will push the numbers 0 through 9 to an array.
+// let numbersArr = []
+// for (let i = 0; i <= 9; i++){
+//     numbersArr.push(i)
+// }
+// console.log(numbersArr)
+
+// // 2. Write a for loop that prints to the console only even numbers 0 through 100.
+// for (let i = 0; i <= 100; i++){
+//     if (i % 2 !== 1)
+//     console.log(i)
+// }
+// // 3. Write a for loop that will push every other fruit to an array.
+
+// const fruit1 = ["banana", "orange", "apple", "kiwi", "pear", "peach"];
+// const everyOtherFruit = [];
+// for (let i = 0; i < fruit1.length; i+=2){
+//     everyOtherFruit.push(fruit1[i]);
+// }
+// console.log(everyOtherFruit)
+// Write a loop that will print out all the names of the people of the people array
+
+const namesArray = [];
+const occupationsArray = [];
+const peopleArray = [
+    {
+      name: "Harrison Ford",
+      occupation: "Actor"
+    },
+    {
+      name: "Justin Bieber",
+      occupation: "Singer"
+    },
+    {
+      name: "Vladimir Putin",
+      occupation: "Politician"
+    },
+    {
+      name: "Oprah",
+      occupation: "Entertainer"
+    }
+  ]
+  
+  // ["Harrison Ford", "Vladimir Putin"] // names
+  // ["Singer", "Entertainer"] // occupation
+//   const namesArray = [];
+//   for (let i = 0; i < peopleArray.name.length; i++){
+//     namesArray.push(peopleArray[i].name)
+//   }
+//   console.log(namesArray)
+for (let i = 0; i < peopleArray.length; i++){
+  namesArray.push(peopleArray[i].name)
+  occupationsArray.push(peopleArray[i].occupation)
+}
+console.log(namesArray, occupationsArray);

883caa2533bfeb4602cdadba08f3c2e6838ab59f
Tyler Walker <boneyardhq@gmail.com>
2024-03-14 13:08:36 -0600

    Grid Practice Assignment - Responsive Grids

--- dom-olympics-start/index.js
+++ dom-olympics-start/index.js
@@ -1,48 +1,38 @@
-const header = document.getElementById("header")
+const header = document.getElementById("header");
 const h1 = document.createElement("h1");
 h1.textContent = "JavaScript Made This!!!";
 header.appendChild(h1);
 h1.className = "header";
 
-
 const subtitle1 = document.createElement("p");
-subtitle1.textContent = "Tyler Walker"
-header.appendChild(subtitle1)
+subtitle1.textContent = "Tyler Walker";
+header.appendChild(subtitle1);
 subtitle1.style.textAlign = "center";
 subtitle1.style.color = "peachpuff";
 subtitle1.style.margin = "auto";
 
 const subtitle2 = document.createElement("span");
-subtitle2.textContent = " wrote the JavaScript!"
+subtitle2.textContent = " wrote the JavaScript!";
 subtitle1.appendChild(subtitle2);
 subtitle2.style.color = "blue";
 
 const messages = document.querySelectorAll(".message");
-for (let i = 0; i < messages.length; i++){
-    messages[i].remove();
-}
 
-const mainDiv = document.querySelector(".messages")
+const mainDiv = document.querySelector(".messages");
 const messageLeft1 = document.createElement("div");
 messageLeft1.textContent = "Hey, how are you?";
 mainDiv.append(messageLeft1);
 messageLeft1.classList.add("message", "left");
 
 const messageRight1 = document.createElement("div");
-messageRight1.textContent = "I'm well, thanks for asking!"
+messageRight1.textContent = "I'm well, thanks for asking!";
 mainDiv.append(messageRight1);
-messageRight1.classList.add("message","right");
+messageRight1.classList.add("message", "right");
 
 const clearButton = document.getElementById("clear-button");
-clearButton.addEventListener("click", function() {
-   const messages = document.querySelectorAll(".message");
-   for (let i = 0; i < messages.length; i++){
+clearButton.addEventListener("click", function () {
+  const messages = document.querySelectorAll(".message");
+  for (let i = 0; i < messages.length; i++) {
     messages[i].remove();
-   }
+  }
 });
-
-
-
-
-
-
--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -1,45 +1,39 @@
-
 const form = document.addItem;
 
-form.addEventListener("submit", function(e){
-    e.preventDefault();
-    const inputItem = form.title.value;
-    form.title.value = ""
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const inputItem = form.title.value;
+  form.title.value = "";
 
-    const listItem = document.createElement("li")
-    const pElement = document.createElement("p")
-    pElement.textContent = inputItem
-    listItem.append(pElement);
-  
+  const listItem = document.createElement("li");
+  const pElement = document.createElement("p");
+  pElement.textContent = inputItem;
+  listItem.append(pElement);
 
-    const deleteButton = document.createElement("button")
-    deleteButton.textContent = "Delete"
-    listItem.append(deleteButton);
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  listItem.append(deleteButton);
 
-    deleteButton.addEventListener("click", function(e){
-        listItem.remove();
-    })
+  deleteButton.addEventListener("click", function (e) {
+    listItem.remove();
+  });
 
-    const editButton = document.createElement("button")
-    editButton.textContent = "Edit"
-    listItem.append(editButton);
+  const editButton = document.createElement("button");
+  editButton.textContent = "Edit";
+  listItem.append(editButton);
 
-    editButton.addEventListener("click", function(e){
-        const editInput= document.createElement("input")
-        const submitEditsButton = document.createElement("button")
-        submitEditsButton.textContent = "submit";
-        listItem.append(submitEditsButton);
-        listItem.append(editInput);
-        submitEditsButton.addEventListener("click", function(){
-         pElement.textContent = editInput.value 
-         editInput.remove();
-         submitEditsButton.remove();  
-        })
-    })
+  editButton.addEventListener("click", function (e) {
+    const editInput = document.createElement("input");
+    const submitEditsButton = document.createElement("button");
+    submitEditsButton.textContent = "submit";
+    listItem.append(submitEditsButton);
+    listItem.append(editInput);
+    submitEditsButton.addEventListener("click", function () {
+      pElement.textContent = editInput.value;
+      editInput.remove();
+      submitEditsButton.remove();
+    });
+  });
 
-  
-
-    document.getElementById("list").append(listItem);
-})
-
-
+  document.getElementById("list").append(listItem);
+});

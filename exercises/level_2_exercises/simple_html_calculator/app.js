be9eeee91c8ee1679601ecad7e05165ab2a1c42c
Tyler Walker <boneyardhq@gmail.com>
2024-03-25 12:36:29 -0600

    finished assignment, answers don't delete, though...they just keep getting added next to the previous answer.

--- exercises/level_2_exercises/simple_html_calculator/app.js
+++ exercises/level_2_exercises/simple_html_calculator/app.js
@@ -1,8 +1,54 @@
-function localScope(){
-  let local;
-  if(2 === 2){
-    local = "dog"
-    console.log(local)
+const addForm = document.getElementById(`add`);
+const subForm = document.getElementById(`subtract`);
+const multForm = document.getElementById(`multiply`);
+
+addButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  const firstAddNumber = parseFloat(addForm.firstAdditionNumber.value);
+  const secondAddNumber = parseFloat(addForm.secondAdditionNumber.value);
+  addForm.firstAdditionNumber.value = "";
+  addForm.secondAdditionNumber.value = "";
+
+  function addTwoNumbers(a, b) {
+    return a + b;
   }
-}
-localScope()
+  let result = addTwoNumbers(firstAddNumber, secondAddNumber);
+  let answerBox = document.createElement("span");
+  answerBox.textContent = result;
+  addForm.append(answerBox);
+  answerBox.classList.add(`answerBox`);
+});
+
+subtractButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  const firstSubNumber = parseFloat(subForm.firstSubtractionNumber.value);
+  const secondSubNumber = parseFloat(subForm.secondSubtractionNumber.value);
+  subForm.firstSubtractionNumber = "";
+  subForm.secondSubtractionNumber = "";
+
+  function subtractTwoNumbers(a, b) {
+    return a - b;
+  }
+  let result = subtractTwoNumbers(firstSubNumber, secondSubNumber);
+  let answerBox = document.createElement("span");
+  answerBox.textContent = result;
+  subForm.append(answerBox);
+  answerBox.classList.add(`answerBox`);
+});
+
+multiplyButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  let firstMultNumber = parseFloat(multForm.firstMultNumber.value);
+  let secondMultNumber = parseFloat(multForm.secondMultNumber.value);
+  multForm.firstMultNumber.value = "";
+  multForm.secondMultNumber.value = "";
+
+  function multiplyTwoNumbers(a, b) {
+    return a * b;
+  }
+  let result = multiplyTwoNumbers(firstMultNumber, secondMultNumber);
+  let answerBox = document.createElement("span");
+  answerBox.textContent = result;
+  multForm.append(answerBox);
+  answerBox.classList.add(`answerBox`);
+});

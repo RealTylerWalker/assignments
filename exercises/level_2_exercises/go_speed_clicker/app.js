f8f19e3fa7d2580cb2c32199db7da7a4ae538f6d
Tyler Walker <boneyardhq@gmail.com>
2024-03-26 17:29:35 -0600

    made some edits with Ben, added an if statement instead of using || 0

--- exercises/level_2_exercises/go_speed_clicker/app.js
+++ exercises/level_2_exercises/go_speed_clicker/app.js
@@ -1,14 +1,17 @@
-function countButtonClicks() {
-  const button = document.getElementById("myButton");
-  const clickCountSpan = document.getElementById("clickCountSpan");
-  let clickCount = sessionStorage.getItem("clickCount") || 0;
-  clickCountSpan.textContent = clickCount;
+const button = document.getElementById("myButton");
+const clickCountSpan = document.getElementById("clickCountSpan");
+let clickCount = sessionStorage.getItem("count");
+// clickCountSpan.textContent = clickCount;
 
-  button.addEventListener("click", function (event) {
-    event.preventDefault();
-    clickCount++;
-    clickCountSpan.textContent = clickCount;
-    sessionStorage.setItem("clickCount", clickCount);
-  });
+if (clickCount) {
+  clickCountSpan.textContent = clickCount;
+} else {
+  clickCountSpan.textContent = 0;
 }
-countButtonClicks();
+
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+  clickCount++;
+  clickCountSpan.textContent = clickCount;
+  sessionStorage.setItem("count", clickCount);
+});

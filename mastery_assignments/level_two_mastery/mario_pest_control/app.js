b880d92d61a11871c5b055c71461b7fb882f8473
Tyler Walker <boneyardhq@gmail.com>
2024-03-26 11:51:02 -0600

    fully functional mario pest control mastery

--- mastery_assignments/level_two_mastery/mario_pest_control/app.js
+++ mastery_assignments/level_two_mastery/mario_pest_control/app.js
@@ -1,8 +1,25 @@
-function localScope(){
-  let local;
-  if(2 === 2){
-    local = "dog"
-    console.log(local)
+const form = document.pricing;
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const totalGoombas = parseFloat(form.goombaInput.value);
+  const goombaPrice = 5;
+  const totalBobOmbs = parseFloat(form.bobOmbInput.value);
+  const bobOmbPrice = 7;
+  const totalCheepCheeps = parseFloat(form.cheepCheepInput.value);
+  const cheepCheepPrice = 11;
+
+  function totalPrice(a, b, c, d, e, f) {
+    return a * b + c * d + e * f;
   }
-}
-localScope()
+  let result = totalPrice(
+    totalGoombas,
+    goombaPrice,
+    totalBobOmbs,
+    bobOmbPrice,
+    totalCheepCheeps,
+    cheepCheepPrice
+  );
+  const priceBox = document.getElementById("priceBox");
+  priceBox.textContent = result;
+});

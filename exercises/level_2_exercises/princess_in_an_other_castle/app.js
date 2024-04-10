311015bab47a29d61a6453a92b0cc24c1c45e028
Tyler Walker <boneyardhq@gmail.com>
2024-04-10 15:16:39 -0600

    Finished Princess In another Castle Assignment

--- exercises/level_2_exercises/princess_in_an_other_castle/app.js
+++ exercises/level_2_exercises/princess_in_an_other_castle/app.js
@@ -1,32 +1,109 @@
-var readlineSync = require("readline-sync");
+const readlineSync = require("readline-sync");
 
-players = ["Mario", "Luigi"];
+// Create statuses
 
-// Asking user which player they want to be
+const POWERED_UP = "Powered-Up";
+const BIG = "Big";
+const SMALL = "Small";
+const DEAD = "Dead";
 
-index = readlineSync.keyInSelect(players, `Which player would you like to be?`);
-
-// Character is assigned
-
-const possibleStates = ["dead", "small", "big", "powered-up"];
+// Create Player Constructur
 
 class Player {
   constructor(name) {
+    // Create player properties ** name, totalCoins, status, hasStar
     this.name = name;
     this.totalCoins = 0;
-    this.status = possibleStates[1];
+    this.status = SMALL;
     this.hasStar = false;
-    // this.setName =
-    // this.gotHit =
-    // this.gotPowerup =
-    // this.addCoin =
-    // this.print =
+  }
+
+  // Create gotHit
+
+  gotHit() {
+    //create if statement that changes player status that levels you down
+    if (this.status === POWERED_UP) {
+      this.status = BIG;
+    } else if (this.status === BIG) {
+      this.status = SMALL;
+    } else if (this.status === SMALL) {
+      this.status = DEAD;
+    }
+  }
+
+  // Create gotPowerup
+
+  gotPowerup() {
+    //create switch that changes player status that levels you up
+    if (this.status === SMALL) {
+      this.status = BIG;
+    } else if (this.status === BIG) {
+      this.status = POWERED_UP;
+    } else if (this.status === POWERED_UP) {
+      this.hasStar = true;
+    }
+  }
+
+  // Add Coin method
+
+  addCoin() {
+    // adds coins to player
+    this.totalCoins = ++this.totalCoins;
+  }
+
+  // create Print method
+
+  print() {
+    console.log(`Name: ${this.name}`);
+    console.log(`Total Coins: ${this.totalCoins}`);
+    console.log(`Status: ${this.status}`);
+    console.log(`Has Star: ${this.hasStar}`);
   }
 }
 
-const mario = new Player("Mario");
-const luigi = new Player("Luigi");
+// create a random number function
 
-// Use a constructor to assign values to properties of the character
+// Create characters
+const characters = ["Mario", "Luigi"];
+// readline-sync key in select
+const characterIndex = readlineSync.keyInSelect(
+  characters,
+  `Which character would you like to be?`
+);
 
-// Create a while loop that keeps the game running so long as the player is not dead
+// Instantiate new players, reference "characters" variable and its index
+const playerInst = new Player(characters[characterIndex]);
+playerInst.print();
+
+// use random number function to switch to different from game actions gotHit, gotPowerup, addCoin
+
+// Create interval function
+const intervalID = setInterval(() => {
+  const roll = Math.floor(Math.random() * 3);
+  console.log(roll);
+  if (roll === 0) {
+    playerInst.gotHit();
+    console.log("You got hit!");
+  }
+  if (roll === 1) {
+    playerInst.gotPowerup();
+    console.log("You powered up!");
+  }
+  if (roll === 2) {
+    playerInst.addCoin();
+    console.log("You got a coin!");
+  }
+  playerInst.print();
+  if (playerInst.status === DEAD) {
+    console.log(`${playerInst.name} has died!`);
+    clearInterval(intervalID);
+  } else if (playerInst.totalCoins >= 2) {
+    console.log(`You won the game as ${characters[characterIndex]}!`);
+    clearInterval(intervalID);
+  }
+}, 1000);
+
+//create method that prints character info
+// if statement that ends game
+
+// clear interval

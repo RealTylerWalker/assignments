ba50e84f1efecd5cb85ec660b4a01d13d3c82147
Tyler Walker <boneyardhq@gmail.com>
2024-04-26 16:25:53 -0600

    functional todo list, CSS could use some work.

--- capstoneProjects/axios_todo_capstone_project_level_2/app.js
+++ capstoneProjects/axios_todo_capstone_project_level_2/app.js
@@ -1,109 +1,12 @@
-// GET's THE TODO's FROM THE DATABASE
-
+//**********  API fetching  ********** */
 const getData = () => {
   axios
     .get("https://api.vschool.io/tylerwalker/todo")
-    .then((response) => listData(response.data))
+    .then((response) => {
+      listData(response.data);
+      renderPricing(response.data);
+    })
     .catch((error) => console.log(error));
-};
-
-// LISTS THE TODO TITLES TO THE DOM
-
-let listData = (data) => {
-  clearList();
-
-  let counter = 1;
-  let projectedPriceValue = 0;
-  let actualPriceValue = 0;
-
-  // Create the totalPrice element
-
-  const projectedTotalPrice = document.createElement("h3");
-  projectedTotalPrice.textContent = "(Projected) Total Price: $0.00";
-  document.body.append(projectedTotalPrice);
-
-  // Create Actual Price Element
-
-  const actualTotalPrice = document.createElement(`h3`);
-  actualTotalPrice.textContent = "(Completed) Total Price: $0.00";
-  document.body.append(actualTotalPrice);
-
-  for (let i = 0; i < data.length; i++) {
-    // create list item elements and then set their values
-
-    const h1Div = document.createElement(`div`);
-    h1Div.classList.add("h1Div");
-
-    const bodyDiv = document.createElement(`div`);
-
-    const h1 = document.createElement(`h1`);
-    h1.textContent = `${counter++}) ${data[i].title}`;
-
-    const span = document.createElement(`span`);
-    span.textContent = `DESCRIPTION: ${data[i].description}`;
-
-    const img = document.createElement(`img`);
-    img.src = data[i].imgUrl;
-
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.id = `completedCheckbox-${data[i]._id}`;
-
-    const priceP = document.createElement("p");
-    priceP.textContent = ` PRICE: $${data[i].price}`;
-
-    const totalPrice = document.createElement("h3");
-
-    //Calculate Total Price
-    projectedPriceValue += parseFloat(data[i].price);
-    //append todo items to the DOM
-
-    h1Div.appendChild(checkbox);
-    h1Div.appendChild(h1);
-    bodyDiv.appendChild(span);
-    bodyDiv.appendChild(priceP);
-    bodyDiv.appendChild(img);
-    bodyDiv.appendChild(deleteButton);
-    document.body.append(totalPrice);
-
-    document.getElementById(`todo-list`).append(h1Div);
-    document.getElementById(`todo-list`).append(bodyDiv);
-
-    const id = data[i]._id;
-    deleteButton.addEventListener("click", (event) => {
-      event.preventDefault();
-      deleteTodo(id);
-    });
-    checkbox.addEventListener(`change`, () => {
-      event.preventDefault();
-      const price = parseFloat(data[i].price);
-      if (event.target.checked) {
-        console.log(h1);
-        h1.style.setProperty(`text-decoration`, `line-through`);
-        span.style.setProperty(`text-decoration`, `line-through`);
-        actualPriceValue += price;
-      } else {
-        h1.style.setProperty(`text-decoration`, `none`);
-        actualPriceValue -= price;
-      }
-      actualTotalPrice.textContent = `(Completed) Total Price $${actualPriceValue.toFixed(
-        2
-      )}`;
-    });
-  }
-  projectedTotalPrice.textContent = `(Projected) Total Price: $${projectedPriceValue.toFixed(
-    2
-  )}`;
-};
-
-const clearList = () => {
-  const el = document.getElementById(`todo-list`);
-  while (el.firstChild) {
-    el.removeChild(el.firstChild);
-  }
 };
 
 const deleteTodo = (id) => {
@@ -113,10 +16,26 @@ const deleteTodo = (id) => {
     .catch((error) => console.log(error));
 };
 
-getData();
+const updateTodo = (id, todo) => {
+  axios
+    .put(`https://api.vschool.io/tylerwalker/todo/${id}`, todo)
+    .then(() => getData())
+    .catch((error) => console.log(error));
+};
 
-// FORM FOR THE POST REQUEST
+const postTodo = (newTodo) => {
+  axios
+    .post("https://api.vschool.io/tylerwalker/todo", newTodo)
+    .then(() => getData())
+    .catch((error) => console.log(error));
+};
+/********************************************** */
 
+/*** Fetch data on load */
+window.addEventListener("load", getData);
+/*************** */
+
+/********* todo form */
 const todoForm = document.todoForm;
 
 todoForm.addEventListener("submit", (event) => {
@@ -133,8 +52,98 @@ todoForm.addEventListener("submit", (event) => {
   todoForm.description.value = "";
   todoForm.imgUrl.value = "";
   todoForm.price.value = "";
-  axios
-    .post("https://api.vschool.io/tylerwalker/todo", newTodo)
-    .then(() => getData())
-    .catch((error) => console.log(error));
+
+  postTodo(newTodo);
 });
+/************************* */
+
+/******** UI renderers */
+
+const renderPricing = (data) => {
+  const projected = data.reduce((total, todo) => todo.price + total, 0);
+  document.getElementById(
+    `projectedTotalPrice`
+  ).textContent = `(Projected) Total Price: $${projected.toFixed(2)}`;
+  const actual = data
+    .filter((todo) => todo.completed)
+    .reduce((total, todo) => todo.price + total, 0);
+  document.getElementById(
+    `actualTotalPrice`
+  ).textContent = `(Completed) Total Price: $${actual.toFixed(2)}`;
+};
+
+let listData = (data) => {
+  clearList();
+  for (let i = 0; i < data.length; i++) {
+    renderTodoItem(data[i], i + 1);
+  }
+};
+
+const renderTodoItem = (todo, itemNumber) => {
+  const todoContainer = document.createElement("div");
+  todoContainer.classList.add("todoContainer");
+
+  const bodyDiv = document.createElement("div");
+  bodyDiv.classList.add("todo-item");
+
+  const h1Div = document.createElement("div");
+  h1Div.classList.add("item-title-div");
+
+  const h1 = document.createElement("h1");
+  h1.textContent = `${itemNumber}) ${todo.title}`;
+  h1.classList.add("todo-item-title");
+
+  const span = document.createElement("span");
+  span.textContent = `DESCRIPTION: ${todo.description}`;
+  span.classList.add("todo-item-details");
+
+  const img = document.createElement("img");
+  img.src = todo.imgUrl;
+  img.classList.add("todo-item-img");
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.classList.add("button-div");
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.id = `completedCheckbox-${todo._id}`;
+  checkbox.classList.add("checkbox");
+  checkbox.checked = todo.completed;
+  if (todo.completed) {
+    h1.classList.add("todo-item-title-checked");
+  } else {
+    h1.classList.remove("todo-item-title-checked");
+  }
+
+  const priceP = document.createElement("p");
+  priceP.textContent = ` PRICE: $${todo.price}`;
+  priceP.classList.add("todo-item-details");
+
+  h1Div.appendChild(checkbox);
+  h1Div.appendChild(h1);
+  bodyDiv.appendChild(span);
+  bodyDiv.appendChild(priceP);
+  bodyDiv.appendChild(img);
+  bodyDiv.appendChild(deleteButton);
+
+  todoContainer.append(h1Div, bodyDiv);
+
+  document.getElementById(`todo-list`).append(todoContainer);
+
+  const id = todo._id;
+  deleteButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    deleteTodo(id);
+  });
+  checkbox.addEventListener("change", () => {
+    updateTodo(id, { ...todo, completed: checkbox.checked });
+  });
+};
+
+const clearList = () => {
+  const el = document.getElementById(`todo-list`);
+  while (el.firstChild) {
+    el.removeChild(el.firstChild);
+  }
+};

16ac2cbeb4861743303b51ef682bd1a8e73723ed
Tyler Walker <boneyardhq@gmail.com>
2024-09-03 11:55:55 -0600

    added context lesson

--- exercises/level_6_exercises/level_6_modules/10_creating_context_for_the_user/client/src/main.jsx
+++ exercises/level_6_exercises/level_6_modules/10_creating_context_for_the_user/client/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter as Router} from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
+import UserProvider from './context/userProvider.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
+    <UserProvider>
       <App />
+    </UserProvider>
   </Router>
 )
--- exercises/level_6_exercises/level_6_modules/10_creating_context_for_the_user/client/vite.config.js
+++ exercises/level_6_exercises/level_6_modules/10_creating_context_for_the_user/client/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     proxy: {
       '/api': {
@@ -13,4 +14,4 @@ export default defineConfig({
       },
     },
   },
-});
+})

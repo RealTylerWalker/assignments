aa7094af83913bce375dccc3e755a4fe75a4770d
Tyler Walker <boneyardhq@gmail.com>
2024-09-18 14:09:56 -0600

    added method to remove passwords from the the user object and the user token

---
+++ exercises/level_6_exercises/level_6_modules/19_handling_login_and_signup_errors/client/src/components/Issue.jsx
@@ -0,0 +1,50 @@
+import { useContext, useState } from 'react'
+import { UserContext } from '../context/UserProvider'
+import EditIssueForm from './EditIssueForm'
+
+export default function Issue(props) {
+
+    const { issue } = props
+
+    const { deleteIssue, editIssue } = useContext(UserContext)
+    const [isBeingEdited, setIsBeingEdited] = useState(false)
+
+
+    function handleEdit() {
+        if (isBeingEdited) {
+            setIsBeingEdited(false)
+        } else {
+            setIsBeingEdited(true)
+        }
+    }
+
+    return (
+        <div>
+            {isBeingEdited ? (<EditIssueForm
+                handleEdit={handleEdit}
+                updateStatus={setIsBeingEdited}
+                issue={props.issue}
+                getIssues={props.getIssues}
+                setForceUpdate={props.setForceUpdate}
+            />) : (<IssueCard
+                issue={issue}
+                handleEdit={handleEdit}
+                deleteIssue={deleteIssue} />)}
+        </div>
+    )
+}
+
+function IssueCard(props) {
+    const { issue: { title, description, imgUrl, _id, userId }, handleEdit, deleteIssue } = props
+    return (<div className="issue-card">
+
+        <h1>Title: {title}</h1>
+        <h4>Description: {description}</h4>
+        <p>User ID: {userId}</p>
+        <p>ID: {_id}</p>
+        <img src={imgUrl} />
+        <button onClick={handleEdit}>Edit</button>
+        <button onClick={() => deleteIssue(_id)}>Delete</button>
+    </div>)
+
+}
